9fc628577c3725d9c7e067352c2a4dccfcc1641b
Lenovo <Lenovo@LAPTOP-N9GJ8RKM>
2022-07-31 15:16:46 +0530

    get1

--- RestAssured1/src/main/java/TestAPI.java
+++ RestAssured1/src/main/java/TestAPI.java
@@ -1,10 +1,23 @@
+import io.restassured.RestAssured;
 
-public class Hi {
+import static io.restassured.RestAssured.*;
+
+public class TestAPI {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hii akash");
+	
+		
+		RestAssured.baseURI="https://rahulshettyacademy.com";
+		
+		given().queryParam("key", "qaclick123")
+	   .header("Content-Type","application/json")
+	   .body("{\"location\":{\"lat\":-38.383494,\"lng\":33.427362},\"accuracy\":50,\"name\":\"Frontlinehouse\",\"phone_number\":\"(+91)9838933937\",\"address\":\"29,sidelayout,cohen09\",\"types\":[\"shoepark\",\"shop\"],\"website\":\"http://google.com\",\"language\":\"French-IN\"}")
+	   .when().post("maps/api/place/add/json")
+	   .then().log().all().assertThat().statusCode(200);
+		
+		
 
 	}
 
